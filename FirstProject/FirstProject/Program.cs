20add4540622fd2ef55749295d1f61de44bce6b7
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-12-10 17:29:16 -0300

    desafio utilizando listas em C#

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -30,8 +30,36 @@ void ExibeMenu()
         case 1:
             Console.WriteLine("Você escolheu a opção " + opcao);
             break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case -1:
+            Console.WriteLine("Tchau Tchau ;)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
 }
 
-MostraMensagem();
-ExibeMenu();
+List<string> lista = ["C#", "JAVA", "JavaScript"];
+
+lista.Add("Python");
+lista.Remove("JAVA");
+
+for (int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine(lista[i]);
+}
+
+//Console.Write("Digite seu nome: ");
+//string nome =  Console.ReadLine();
+//Console.Write("Digite seu Sobrenome: ");
+//string sobrenome = Console.ReadLine();
+//Console.WriteLine($"Olá {nome} {sobrenome}, seja muito bem-vindo!");
