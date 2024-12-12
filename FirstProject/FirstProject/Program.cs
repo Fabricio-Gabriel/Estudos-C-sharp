2feeed4853e46cca85d55038d27d23ce92c005ec
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-12-12 15:22:04 -0300

    Criando funções do menu

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,123 +1,115 @@
-﻿//string msgBoasVindas = "Boas vindas ao Screen Sound";
-//void MostraMensagem()
-//{
-//    Console.WriteLine(@"
-
-//░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-//██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-//╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-//░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-//██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-//╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-//");
-//    Console.WriteLine(msgBoasVindas + "\n");
-
-//}
-
-//void ExibeMenu()
-//{
-//    Console.WriteLine("Digite 1 registrar uma banda");
-//    Console.WriteLine("Digite 2 para mostrar todas as bandas");
-//    Console.WriteLine("Digite 3 para avaliar uma banda");
-//    Console.WriteLine("Digite 4 para exibir a média de uma banda");
-//    Console.WriteLine("Digite -1 para sair");
-
-//    Console.Write("\nDigite a sua opção: ");
-//    int opcao = int.Parse(Console.ReadLine())!;
-
-//    switch(opcao)
-//    {
-//        case 1:
-//            Console.WriteLine("Você escolheu a opção " + opcao);
-//            break;
-//        case 2:
-//            Console.WriteLine("Você escolheu a opção " + opcao);
-//            break;
-//        case 3:
-//            Console.WriteLine("Você escolheu a opção " + opcao);
-//            break;
-//        case 4:
-//            Console.WriteLine("Você escolheu a opção " + opcao);
-//            break;
-//        case -1:
-//            Console.WriteLine("Tchau Tchau ;)");
-//            break;
-//        default:
-//            Console.WriteLine("Opção inválida");
-//            break;
-//    }
-//}
-
-int VerificaNum(int escolha)
+﻿string msgBoasVindas = "Boas vindas ao Screen Sound";
+void ExibeLogo()
 {
-    if (escolha < 0 || escolha > 100)
-    {
-        Console.WriteLine("Escolha inválida, Tente novamente (0 a 100)");
-        return -1;
-    }
-    else
-    {
-        return escolha;
-    }
-  
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(msgBoasVindas + "\n");
 
 }
 
-Random aleatorio = new Random();
-
-int valorMaquina = aleatorio.Next(100);
-
-Console.WriteLine("========================================");
-Console.WriteLine("SEJA BEM-VINDO AO JOGO DA ADVINHAÇÃO!!!");
-Console.WriteLine("========================================\n");
-
-do
+void ExibeMenu()
 {
-    Console.Write("Digite o número que escolheu: ");
-    int escolha = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite 1 registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
 
-    if(VerificaNum(escolha) < 0 || VerificaNum(escolha) > 100)
+    Console.Write("\nDigite a sua opção: ");
+    int opcao = int.Parse(Console.ReadLine())!;
+
+    switch (opcao)
     {
-        continue;
+        case 1:
+            RegistrarBandas();
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcao);
+            break;
+        case -1:
+            Console.WriteLine("Tchau Tchau ;)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
-    
-    if (VerificaNum(escolha) < valorMaquina)
-    {
-        Console.WriteLine("O número digitado é menor do que o escolhido, tente de novo!\n");
-        continue;
-    } else if (VerificaNum(escolha) > valorMaquina)
-    {
-        Console.WriteLine("O número digitado é maior que o escolhido, tente de novo!\n");
-        continue;
-    } else
-    {
-        Console.WriteLine("Parabéns! Você Acertou!\n");
-        Console.WriteLine($"O valor digitado foi {VerificaNum(escolha)} o valor escolhido foi {valorMaquina}");
-        break;
-    }
+}
 
-} while (true);
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Bandas\n");
+    Console.Write("Digite o nome da banda que deseja registar: ");
+    string nomeBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeBanda} foi registrada!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibeLogo();
+    ExibeMenu();
+}
 
+ExibeLogo();
+ExibeMenu();
 
-
-
-
-
-
-
-
-//List<string> lista = ["C#", "JAVA", "JavaScript"];
-
-//lista.Add("Python");
-//lista.Remove("JAVA");
-
-//for (int i = 0; i < lista.Count; i++)
+//int VerificaNum(int escolha)
 //{
-//    Console.WriteLine(lista[i]);
+//    if (escolha < 0 || escolha > 100)
+//    {
+//        Console.WriteLine("Escolha inválida, Tente novamente (0 a 100)");
+//        return -1;
+//    }
+//    else
+//    {
+//        return escolha;
+//    }
+
+
 //}
 
-//Console.Write("Digite seu nome: ");
-//string nome =  Console.ReadLine();
-//Console.Write("Digite seu Sobrenome: ");
-//string sobrenome = Console.ReadLine();
-//Console.WriteLine($"Olá {nome} {sobrenome}, seja muito bem-vindo!");
+//Random aleatorio = new Random();
+
+//int valorMaquina = aleatorio.Next(100);
+
+//Console.WriteLine("========================================");
+//Console.WriteLine("SEJA BEM-VINDO AO JOGO DA ADVINHAÇÃO!!!");
+//Console.WriteLine("========================================\n");
+
+//do
+//{
+//    Console.Write("Digite o número que escolheu: ");
+//    int escolha = int.Parse(Console.ReadLine());
+
+//    if(VerificaNum(escolha) < 0 || VerificaNum(escolha) > 100)
+//    {
+//        continue;
+//    }
+
+//    if (VerificaNum(escolha) < valorMaquina)
+//    {
+//        Console.WriteLine("O número digitado é menor do que o escolhido, tente de novo!\n");
+//        continue;
+//    } else if (VerificaNum(escolha) > valorMaquina)
+//    {
+//        Console.WriteLine("O número digitado é maior que o escolhido, tente de novo!\n");
+//        continue;
+//    } else
+//    {
+//        Console.WriteLine("Parabéns! Você Acertou!\n");
+//        Console.WriteLine($"O valor digitado foi {VerificaNum(escolha)} o valor escolhido foi {valorMaquina}");
+//        break;
+//    }
+
+//} while (true);
