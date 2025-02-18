d09827e2fca154ad1f2d47f7f94dce5cbecfffa4
Fabricio Gabriel <fabricio.melquiades04@gmail.com>
2025-02-18 16:22:46 -0300

    refatorando código com lambda

--- FirstProject/FirstProject/Musica.cs
+++ FirstProject/FirstProject/Musica.cs
@@ -1,14 +1,37 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    private bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel {  get; set; }
+    public string DescricaoResumida 
+        => $"A música {this.Nome} pertence à banda {this.Artista}";
 
 
     public void ExibeFichaTecnica()
     {
+        Console.WriteLine();
+        Console.WriteLine($"Nome: {this.Nome}");
+        Console.WriteLine($"Artista: {this.Artista}");
+        Console.WriteLine($"Duração: {this.Duracao}");
 
+        if (this.Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
     }
+
+    //public void setDisponivel(bool value)
+    //{
+    //    this.disponivel = value;
+    //}
+
+    //public bool getDisponivel()
+    //{
+    //    return this.disponivel;
+    //}
 
 }
--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -213,19 +213,20 @@
 Musica musica1 = new Musica();
 Musica musica2 = new Musica();
 
-musica1.nome = "Roxane";
-musica1.artista = "The police";
-musica1.duracao = 273;
-musica1.disponivel = true;
-//Console.WriteLine(musica1.DescricaoResumida);
+musica1.Nome = "Roxane";
+musica1.Artista = "The police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
 
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
-musica2.disponivel = false;
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.DescricaoResumida);
 
-//musica1.exibeFicha();
-//musica2.exibeFicha();
+musica1.ExibeFichaTecnica();
+musica2.ExibeFichaTecnica();
 
 //Carro car1 = new Carro();
 
